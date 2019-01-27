6e213a40670a09a54d50081e6deb83f2b3a31296
jungbae seo <hiwattc@gmail.com>
2019-01-27 21:40:36 +0900

    1st commit

---
+++ src/main/java/com/staroot/BootTrainingMybatisApplication.java
@@ -0,0 +1,14 @@
+package com.staroot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootTrainingMybatisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootTrainingMybatisApplication.class, args);
+	}
+
+}
+
