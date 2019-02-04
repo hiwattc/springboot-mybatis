e857740c3b19dcf53f10deb27dc9701bea4fba5d
jungbae seo <hiwattc@gmail.com>
2019-02-04 12:50:31 +0900

    19.02.04 commit

--- src/main/java/com/staroot/BootTrainingMybatisApplication.java
+++ src/main/java/com/staroot/BootTrainingMybatisApplication.java
@@ -1,12 +1,17 @@
 package com.staroot;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BootTrainingMybatisApplication {
 
+	private static final Logger logger = LoggerFactory.getLogger(BootTrainingMybatisApplication.class);
+	
 	public static void main(String[] args) {
+		logger.debug("BootTrainingMybatisApplication start");
 		SpringApplication.run(BootTrainingMybatisApplication.class, args);
 	}
 
