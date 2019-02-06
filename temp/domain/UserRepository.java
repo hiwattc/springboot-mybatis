2f6367e62971002f3ecc35bd80a94adc95041603
jungbae seo <hiwattc@gmail.com>
2019-02-06 13:28:39 +0900

    (19.02.06) commit

--- temp/domain/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.staroot.mybatis.domain;
-
-import java.io.Serializable;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Serializable>{
-	User findByUserId(String userId);
-}
