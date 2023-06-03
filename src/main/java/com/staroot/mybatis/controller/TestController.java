afdeadc0ff10e151bef8703e21f3b01ff70bb859
hiwattc <hiwattc@gmail.com>
2023-06-03 21:15:05 +0900

    2023.06.03

--- src/main/java/com/staroot/mybatis/controller/TestController.java
+++ src/main/java/com/staroot/mybatis/controller/TestController.java
@@ -84,6 +84,7 @@ public class TestController {
 		logger.debug("ffffff :: called function : getTest4");
 		logger.debug("ffffff :: called function : getTest4");
 		logger.debug("ffffff :: called function : getTest4");
+		logger.debug("ffffff :: called function : getTest4");
 		return "index";
 
 	}
